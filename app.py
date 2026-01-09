3afbf5dc9f484022c376e4b2df33eaf8c0e5a251
qkrtlwls123-a11y <qkrtlwls123@gmail.com>
2026-01-09 18:17:59 +0900

    Update app.py

--- app.py
+++ app.py
@@ -52,5 +52,5 @@ def config_view():
     return render_template("config.html", data=data)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
